b6d6243f627543fc1d547c723afc366be46d910a
amaan-fps <87180951+amaan-fps@users.noreply.github.com>
2026-05-10 00:52:28 +0530

    Updated for New Site Changes

--- requests_check.py
+++ requests_check.py
@@ -6,25 +6,27 @@ def get_json_with_requests(url):
     }
 
     headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8',
-        'cache-control': 'max-age=0',
-        'priority': 'u=0, i',
-        'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Microsoft Edge";v="140"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        # 'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Referer': 'https://www.olx.in/gurgaon_g4058748/q-ps4',
+        # 'X-Panamera-fingerprint': 'cd4b6e9eec9bb3bddf32fd68ccd7b30e#1778352737917',
+        'Origin': 'https://www.olx.in',
+        'Connection': 'keep-alive',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'Priority': 'u=0',
+        # Requests doesn't support trailers
+        # 'TE': 'trailers',
     }
 
-    response = requests.get(url, cookies=cookies, headers=headers)
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
-
+    # print(response.text)
     return response.json()
 
 if __name__ == '__main__':
-    get_json_with_requests("https://www.olx.in/api/relevance/v4/search?facet_limit=1000&lang=en-IN&location=4058748&location_facet_limit=40&platform=web-desktop&pttEnabled=true&query=ps4&relaxedFilters=true&size=500&spellcheck=true&user=005009916225510713&price_max=13000&price_min=3000")
+    get_json_with_requests("https://api.olx.in/relevance/v4/search?facet_limit=1000&lang=en-IN&location=4058748&location_facet_limit=40&platform=web-desktop&pttEnabled=true&query=ps4&relaxedFilters=true&size=500&spellcheck=true&user=005009916225510713&page=1&price_max=35000&price_min=12000")
     print("exiting!")
